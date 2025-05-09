6249ae246e4e8e05d521f7cf62b2507bdca1386b
Ancous <111791875+Ancous@users.noreply.github.com>
2025-05-09 18:37:52 +0300

    commit_12

--- router.py
+++ router.py
@@ -1,19 +1,20 @@
 from fastapi import APIRouter
 from typing import Annotated
 
-from schemas import STaskAdd
+from schemas import STaskAdd, STask, STaskId
 from repository import TaskRepository
 
 router = APIRouter(
-  prefix="/tasks"
+  prefix="/tasks",
+  tags=["Таски"]
 )
 
 @router.post("")
-async def add_task(task: Annotated[STaskAdd, Depends()]):
+async def add_task(task: Annotated[STaskAdd, Depends()]) -> STaskId:
   task_id = await TaskRepository.add_one(task)
   return {"ok": True, "task_id": task_id}
 
 @router.get("")
-async def get_home():
+async def get_home() -> list[STask]:
   tasks = await TaskRepository.find_all()
-  retutn {"tasks": tasks}
+  retutn tasks
