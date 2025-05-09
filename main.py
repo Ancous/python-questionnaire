7f8074401ebb58386d9aa7f526b63e594ede671f
Ancous <111791875+Ancous@users.noreply.github.com>
2025-05-09 17:36:35 +0300

    commit_4

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from typing import Annotated
 from pydantic import BaseModel
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from database import delete_tables, create_tables
+
+@asynccontextmanager
+async def lifespan():
+  await delete_tables()
+  print("База очищена")
+  await create_tables()
+  print("База готова к работе")
+  yield
+  print("Выключение")
+
+app = FastAPI(lifespan=lifespan)
 
 class STaskAdd(BaseModel):
   name: str
