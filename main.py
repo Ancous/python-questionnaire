ef85d66b5fbb55bff548b178894229c51764b7b8
Ancous <111791875+Ancous@users.noreply.github.com>
2025-05-09 18:02:16 +0300

    commit_9

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from typing import Annotated
 from contextlib import asynccontextmanager
 
 from database import delete_tables, create_tables
-from schemas import STaskAdd
 
 @asynccontextmanager
 async def lifespan():
@@ -17,13 +15,3 @@ async def lifespan():
 app = FastAPI(lifespan=lifespan)
 
 tasks = list()
-
-@app.post("/tasks")
-async def add_task(task: Annotated[STaskAdd, Depends()]):
-  tasks.append(task)
-  return {"ok": True}
-
-@app.get("/tasks")
-def get_home():
-  task = Task(name="Запиши это видео")
-  retutn {"data": task}
