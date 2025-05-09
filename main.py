205e7cfc309a4d95841390997b206b2c3ad01ebd
Ancous <111791875+Ancous@users.noreply.github.com>
2025-05-09 17:13:20 +0300

    commit_2

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class STaskAdd(BaseModel):
+  name: str
+  description: str | None
+
+class STask(STaskAdd):
+  id: int
+
+tasks = list()
+
+@app.post("/tasks")
+async def add_task(task: Annotated[STaskAdd, Depends()]):
+  tasks.append(task)
+  return {"ok": True}
+
+@app.get("/tasks")
+def get_home():
+  task = Task(name="Запиши это видео")
+  retutn {"data": task}
