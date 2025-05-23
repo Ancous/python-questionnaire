10f9efd56203af3583a977c83d2053886a557f3e
alex <alex_taras@bk.ru>
2025-05-23 23:52:09 +0300

    commit_19

--- app.py
+++ app.py
@@ -2,9 +2,28 @@
 Документация модуля
 """
 
+import secrets
 
-from pet-project import app
+from flask import Flask
 
+from route.api.route_table_question_update import api_tables_questions_bp
+from route.api.route_tables_all_remove import api_tables_bp
+from route.web.route_answer import answer_bp
+from route.web.route_main import main_bp
+from route.web.route_questions import questions_bp
+from route.web.route_questions_update import questions_update_bp
+from route.web.route_statistics import statistics_bp
+
+app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+
+app.register_blueprint(api_tables_questions_bp)
+app.register_blueprint(api_tables_bp)
+app.register_blueprint(answer_bp)
+app.register_blueprint(main_bp)
+app.register_blueprint(questions_bp)
+app.register_blueprint(questions_update_bp)
+app.register_blueprint(statistics_bp)
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000, debug=True)
