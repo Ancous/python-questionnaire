cef08a2edf30e56ea01626daa77c251f75e03e8a
alex <alex_taras@bk.ru>
2025-07-03 22:42:26 +0300

    commit_61

--- update_answer.py
+++ update_answer.py
@@ -4,12 +4,10 @@ pass
 
 import re
 from pathlib import Path
-import urllib.parse
-from typing import List
-
-import markdown2
 from sqlalchemy import create_engine, select, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from app.utils.processing_data import extract_decode_answer_link, parse_answer_file
 
 POSTGRES_URL = "postgresql://root:1975@31.129.100.106:5432/python_interview"
 QUESTION_PATTERN = re.compile(
@@ -38,40 +36,6 @@ class Answers(Base):
     question_id = Column(Integer)
 
 
-def filter_irrelevant_lines(lines: List[str]) -> str:
-    """
-    Очищает строки ответа от лишних разметок и объединяет их в одну строку.
-    """
-    filtered_lines = ""
-    for line_2 in lines:
-        if not line_2.strip().startswith(IGNORE_LINE_PREFIXES) and line_2:
-            filtered_lines += line_2
-    return filtered_lines
-
-
-def parse_answer_file(filepath: Path) -> str:
-    """
-    Читает файл-ответ, чистит лишние строки, форматирует code-блоки и превращает markdown в html.
-    """
-    with filepath.open(encoding='utf-8') as file:
-        lines = file.readlines()
-
-    filtered = filter_irrelevant_lines(lines)
-    return markdown2.markdown(filtered, extras=["fenced-code-blocks"]).strip()
-
-
-def extract_decode_answer_link(line: str) -> str:
-    """
-    Извлекает и декодирует ссылку на файл-ответ из строки.
-    """
-    start = line.find(ANSWER_LINK_PREFIX) + len(ANSWER_LINK_PREFIX)
-    end = line.find(ANSWER_LINK_SUFFIX, start)
-    if start == -1 + len(ANSWER_LINK_PREFIX) or end == -1:
-        raise ValueError("Невалидная строка со ссылкой ответа.")
-    encoded_link = line[start:end]
-    return urllib.parse.unquote(encoded_link)
-
-
 def main(se):
     """
     Документация функции
@@ -97,4 +61,5 @@ def main(se):
 if __name__ == '__main__':
     engine = create_engine(POSTGRES_URL)
     Session = sessionmaker(engine)
+
     main(Session)
